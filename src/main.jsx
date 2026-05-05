138fd1f74b2ae87eb55ae64f7cdfc406918530c3
diariolunar <diariolunar47@gmail.com>
2026-05-05 11:38:24 -0300

    Comiit

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+document.getElementById("root").innerHTML = "<h1>React ainda não, mas JS funcionou</h1>";
